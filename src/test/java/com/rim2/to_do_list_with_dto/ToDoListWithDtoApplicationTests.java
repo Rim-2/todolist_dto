bfd44c784ae19218dd36680253b8b96922517b11
Rim-2 <turtle2260@gmail.com>
2025-07-10 16:17:16 +0900

    first commit

---
+++ src/test/java/com/rim2/to_do_list_with_dto/ToDoListWithDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rim2.to_do_list_with_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListWithDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
